c84cfca8b34f8611d78f66b3dcef1cd204365f0f
olli <oliver.zeigermann@gmail.com>
2013-01-03 17:39:35 +0100

    Stripped to starting version

--- war/src/restCalculator.js
+++
@@ -1,5 +0,0 @@
-angular.module('restclient', ['ngResource']).
-    factory('RestCalculator', function($resource) {
-        var RestCalculator = $resource('/mortgage_calculator/rs/:price/:down/:interest/:term');
-        return RestCalculator;
-    });
